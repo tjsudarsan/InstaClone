a841ec926266af967ffdc9f5cbf30cfe00e98c03
tjsudarsan <tjsudarsan@gmail.com>
2020-03-16 20:38:51 +0530

    initial Comit

---
+++ src/containers/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/Login.scss';
+
+const Login = (props) => (
+  <div className="row align-items-center login-container">
+    <div className="d-none d-md-block col-md-8 text-center info-container">
+      <h1>Instagram</h1>
+    </div>
+    <div className="col-12 col-md-4 text-center login-form-container">
+      <h1>Form</h1>
+    </div>
+  </div>
+);
+
+export default Login;
